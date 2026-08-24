ef88650053962e9ddb83f48880b7f0c8bff3ae02
songjian <songjiansj52@163.com>
2026-08-24 13:39:52 +0800

    matchForBest补充剔除关键字

--- src/main/java/com/changgeng/controller/CommonController.java
+++ src/main/java/com/changgeng/controller/CommonController.java
@@ -9,9 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static org.apache.commons.collections4.CollectionUtils.collect;
@@ -25,6 +23,10 @@ public class CommonController {
 
     @Autowired
     DamExtClient damExtClient;
+
+    private static final Set<String> NOISE = new HashSet<>(Arrays.asList("运行状况","运行情况","运行状态","运行怎",
+            "情况","状态","如何","怎么样","怎样","最近","今天","昨天","健康","评估",
+            "有没有","问题","是否","正常","时间","想","下","看","查","一下")) ;
 
     /**
      * 根据输入的匹配字符串，从所有实例中模糊匹配出相似度最高的前num个实例。
@@ -46,6 +48,10 @@ public class CommonController {
                     return o;
                 })
                 .collect(Collectors.toList());
+
+
+        for (String w : NOISE)
+            matchString = matchString.replace(w, "");
         List<Map> matchedStr =  CommonTool.getBestMatchingStr(instanceList,matchString, num);
         log.info("matchedResult: {}", matchedStr);
         return Result.success(matchedStr);
